65f3107a80efc3d34d51da62f03055f545d6ac01
Enan Ajmain <3nan.ajmain@gmail.com>
2022-02-02 01:54:58 +0600

    Don't send dim_input as class variable

--- cnn.py
+++ cnn.py
@@ -29,22 +29,6 @@ def zero_pad(X, pad):
 
 
 
-class LayerFullyConnected:
-    def __init__(self, dim_input, dim_output):
-        self.dim_input = dim_input.astype(int)
-        self.dim_output = dim_output.astype(int)
-
-        self.weights = np.random.randn(self.num_filters, self.dim_input[2], *self.dim_filters)
-        self.biases = np.random.rand(self.num_filters,1)
-
-        self.weights = np.random.randn(self.dim_output, *self.dim_input)
-        self.biases = np.random.randn(*self.dim_output,1)
-
-    def forward(self, input_data):
-        self.weights = self.weights.reshape((self.dim_output, np.prod(self.dim_input)))
-        self.input_data = input_data.reshape((np.prod(dim_input), 1))
-        output = np.dot(self.weights, input_data) + self.biases
-
 class LayerFlattening:
     def __init__(self, dim_input):
         self.dim_input = dim_input.astype(int)
@@ -54,25 +38,27 @@ class LayerFlattening:
 
 
 class LayerConvolution:
-    def __init__(self, dim_input, num_filters, dim_filters, stride, padding):
-        self.dim_input = dim_input
+    def __init__(self, num_filters, dim_filters, stride, padding):
         self.num_filters = num_filters
         self.dim_filters = dim_filters.astype(int)
         self.stride = stride
         self.padding = padding
 
-        self.weights = np.random.randn(self.num_filters, self.dim_input[2], *self.dim_filters)
-        self.biases = np.random.rand(self.num_filters,1)
-
-        self.dim_output = (self.dim_input[:-1] - self.dim_filters + 2*self.padding) / self.stride + 1
-        self.dim_output = self.dim_output.astype(int)
 
     def forward(self, input_neurons):
-        input_neurons = zero_pad(input_neurons, self.padding)
         print(input_neurons.shape)
         num_neurons = input_neurons.shape[0]
+        dim_input = input_neurons.shape[1:]
+        input_neurons = zero_pad(input_neurons, self.padding)
+
+        self.weights = np.random.randn(self.num_filters, dim_input[-1], *self.dim_filters)
+        self.biases = np.random.rand(self.num_filters,1)
+
+        self.dim_output = (dim_input[:-1] - self.dim_filters + 2*self.padding) / self.stride + 1
+        self.dim_output = self.dim_output.astype(int)
         output = np.zeros((num_neurons, *self.dim_output, self.num_filters))
         output = output.reshape((num_neurons, np.prod(self.dim_output), self.num_filters))
+
         for k in range(num_neurons):
             for j in range(self.num_filters):
                 col = 0
@@ -84,7 +70,7 @@ class LayerConvolution:
                                                   col:self.dim_filters[1]+col,
                                                   :], self.weights[j])) + self.biases[j]
                     col += self.stride
-                    if col + self.dim_filters[0] > self.dim_input[1]:
+                    if col + self.dim_filters[0] > dim_input[1]:
                         col = 0
                         row += self.stride
         output = output.reshape((num_neurons, *self.dim_output, self.num_filters))
@@ -92,21 +78,23 @@ class LayerConvolution:
 
 
 class LayerMaxPooling:
-    def __init__(self, dim_input, dim_filters, stride):
-        self.dim_input = dim_input
+    def __init__(self, dim_filters, stride):
         self.dim_filters = dim_filters.astype(int)
         self.stride = stride
 
-        self.dim_output = (self.dim_input[:-1] - self.dim_filters) / self.stride + 1
+        self.dim_output = (dim_input[:-1] - self.dim_filters) / self.stride + 1
         self.dim_output = self.dim_output.astype(int)
 
     def forward(self, input_neurons):
         print(input_neurons.shape)
         num_neurons = input_neurons.shape[0]
-        output = np.zeros((num_neurons, *self.dim_output, self.dim_input[-1]))
-        output = output.reshape((num_neurons, np.prod(self.dim_output), self.dim_input[-1]))
+        dim_input = input_neurons.shape[1:]
+
+        output = np.zeros((num_neurons, *self.dim_output, dim_input[-1]))
+        output = output.reshape((num_neurons, np.prod(self.dim_output), dim_input[-1]))
+
         for k in range(num_neurons):
-            for j in range(self.dim_input[-1]):
+            for j in range(dim_input[-1]):
                 row = 0
                 col = 0
                 for i in range(np.prod(self.dim_output)):
@@ -114,17 +102,35 @@ class LayerMaxPooling:
                                           col:self.dim_filters[0]+col, j]
                     output[k][i][j] = np.amax(slide)
                     col += self.dim_filters[1]
-                    if col + self.dim_filters[1] > self.dim_input[1]:
+                    if col + self.dim_filters[1] > dim_input[1]:
                         col = 0
                         row += self.dim_filters[0]
-        output = output.reshape((num_neurons, *self.dim_output, self.dim_input[-1]))
+        output = output.reshape((num_neurons, *self.dim_output, dim_input[-1]))
         return output
+
+
+class LayerFullyConnected:
+    def __init__(self, num_output):
+        self.num_output = num_output
+
+    def forward(self, input_neurons):
+        self.weights = np.random.randn(self.num_output, *self.dim_input)
+        self.biases = np.random.randn(self.num_output,1)
+        self.weights = self.weights.reshape((self.num_output, np.prod(self.dim_input)))
+        self.input_neurons = input_neurons.reshape((np.prod(self.dim_input), 1))
+        output = np.dot(self.weights, input_neurons) + self.biases
+        return output
+
+
+
 
 if __name__ == "__main__":
     x_train, y_train, x_valid, y_valid, x_test, y_test = load_mnist()
     dim_image = tuple(np.append(np.array(x_train[0].shape), 1))
     z = np.expand_dims(x_train, 3)
-    layer1 = LayerConvolution(dim_image, 6, np.array((5,5)), 1, 2)
-    o1 = layer1.forward(z[:1000])
-    layer2 = LayerMaxPooling(o1.shape[1:], np.array((2,2)), 2)
-    o2 = layer2.forward(o1)
+    l1 = LayerConvolution(6, np.array((5,5)), 1, 2)
+    o1 = l1.forward(z[:100])
+    l2 = LayerMaxPooling(np.array((2,2)), 2)
+    o2 = l2.forward(o1)
+    l3 = LayerFullyConnected(10)
+    o3 = l3.forward(o2)
